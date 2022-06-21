323cb39ea0ea5b5d0f9b3241c5fe428e63b46213
janak <krja1031@h-ka.de>
2022-06-21 16:02:58 +0200

    Uebungen zugefügt

--- app/src/main/java/com/example/fitworkmockup/DBHelper.java
+++ app/src/main/java/com/example/fitworkmockup/DBHelper.java
@@ -87,41 +87,41 @@ public class DBHelper extends SQLiteOpenHelper {
         String mTrue = "true"; String mFalse = "false";
 
         //Übung 1
-        int bild1 = R.drawable.spazieren_fitwork;
-        int gif1 = R.drawable.spazieren_gif;
+        int ansagen_bild = R.drawable.ansagen_bild;
+        int ansagen_gif = R.drawable.ansagen_gif;
         ContentValues values1 = new ContentValues();
-        values1.put(COLUMN_NAME_UEBUNGSNAME, "Spazieren");
-        values1.put(COLUMN_NAME_UEBUNGSBESCHREIBUNG, "Geh raus an die frische Luft und laufe durch die Gegend");
-        values1.put(COLUMN_NAME_GIF, Integer.toString(gif1)); //echt auf Tippfehler aufpassen - am besten Copy&Paste vom Namen und dann ".gif" löschen
-        values1.put(COLUMN_NAME_BILD, Integer.toString(bild1)); //siehe GIF
+        values1.put(COLUMN_NAME_UEBUNGSNAME, "Koordination");
+        values1.put(COLUMN_NAME_UEBUNGSBESCHREIBUNG, "Im Stand werden Arme und Beine in bestimmten Variationen entweder auf derselben oder der gegenüberliegenden Körperseite nach Ansage bewegt");
+        values1.put(COLUMN_NAME_GIF, Integer.toString(ansagen_gif)); //echt auf Tippfehler aufpassen - am besten Copy&Paste vom Namen und dann ".gif" löschen
+        values1.put(COLUMN_NAME_BILD, Integer.toString(ansagen_bild)); //siehe GIF
         values1.put(COLUMN_NAME_KOERPERTEIL, mGanzkoerper);
-        values1.put(COLUMN_NAME_SCHWIERIGKEIT, "1"); //Schwierigkeit als Zahl - 1 = Leicht, 2 = Mittel, 3 = Schwer
+        values1.put(COLUMN_NAME_SCHWIERIGKEIT, "1-2"); //Schwierigkeit als Zahl - 1 = Leicht, 2 = Mittel, 3 = Schwer
         values1.put(COLUMN_NAME_WIEDERHOLUNG, mFalse); // Wenn die Übung mit Wiederholungen ausgeübt wird (wie z.B. Kniebeugen) = true; wenn nicht (z.B. Spazieren) = false
-        values1.put(COLUMN_NAME_PARTNER, mFalse);
+        values1.put(COLUMN_NAME_PARTNER, mTrue);
         db.insert(TABLE_WORKOUTS, null, values1);
 
         //Übung 2
-        int bild2 = R.drawable.fitwork_beispielbild;
-        int gif2 = R.drawable.bsp_gif;
+        int armdruecken_bild = R.drawable.armdruecken_bild;
+        int armdruecken_gif = R.drawable.armdruecken_gif;
         ContentValues values2 = new ContentValues();
-        values2.put(COLUMN_NAME_UEBUNGSNAME, "Mega Hypnose");
-        values2.put(COLUMN_NAME_UEBUNGSBESCHREIBUNG, "Komisches GIF");
-        values2.put(COLUMN_NAME_GIF, Integer.toString(gif2)); //echt auf Tippfehler aufpassen - am besten Copy&Paste vom Namen und dann ".gif" löschen
-        values2.put(COLUMN_NAME_BILD, Integer.toString(bild2)); //siehe GIF
-        values2.put(COLUMN_NAME_KOERPERTEIL, mMental);
-        values2.put(COLUMN_NAME_SCHWIERIGKEIT, "3"); //Schwierigkeit als Zahl - 1 = Leicht, 2 = Mittel, 3 = Schwer
+        values2.put(COLUMN_NAME_UEBUNGSNAME, "Armdrücken");
+        values2.put(COLUMN_NAME_UEBUNGSBESCHREIBUNG, "Beide Personen sitzen sich an einem Tisch direkt gegenüber. Ihre Ellbogen liegen auf der Tischplatte auf und dürfen diese während des gesamten Duells nicht verlassen.");
+        values2.put(COLUMN_NAME_GIF, Integer.toString(armdruecken_gif)); //echt auf Tippfehler aufpassen - am besten Copy&Paste vom Namen und dann ".gif" löschen
+        values2.put(COLUMN_NAME_BILD, Integer.toString(armdruecken_bild)); //siehe GIF
+        values2.put(COLUMN_NAME_KOERPERTEIL, mArme);
+        values2.put(COLUMN_NAME_SCHWIERIGKEIT, "2"); //Schwierigkeit als Zahl - 1 = Leicht, 2 = Mittel, 3 = Schwer
         values2.put(COLUMN_NAME_WIEDERHOLUNG, mFalse); // Wenn die Übung mit Wiederholungen ausgeübt wird (wie z.B. Kniebeugen) = true; wenn nicht (z.B. Spazieren) = false
-        values2.put(COLUMN_NAME_PARTNER, mFalse);
+        values2.put(COLUMN_NAME_PARTNER, mTrue);
         db.insert(TABLE_WORKOUTS, null, values2);
 
         //Übung 3
-        int bild3 = R.drawable.fitwork_beispielbild;
-        int gif3 = R.drawable.bsp_gif2;
+        int atmen_bild = R.drawable.atmen_bild;
+        int atmen_gif = R.drawable.atmen_gif;
         ContentValues values3 = new ContentValues();
-        values3.put(COLUMN_NAME_UEBUNGSNAME, "Rückendehnung");
-        values3.put(COLUMN_NAME_UEBUNGSBESCHREIBUNG, "Blah");
-        values3.put(COLUMN_NAME_GIF, Integer.toString(gif3)); //echt auf Tippfehler aufpassen - am besten Copy&Paste vom Namen und dann ".gif" löschen
-        values3.put(COLUMN_NAME_BILD, Integer.toString(bild3)); //siehe GIF
+        values3.put(COLUMN_NAME_UEBUNGSNAME, "Achtsames Atmen");
+        values3.put(COLUMN_NAME_UEBUNGSBESCHREIBUNG, "Tief und ruhig in die Bauchgegend atmen");
+        values3.put(COLUMN_NAME_GIF, Integer.toString(atmen_gif)); //echt auf Tippfehler aufpassen - am besten Copy&Paste vom Namen und dann ".gif" löschen
+        values3.put(COLUMN_NAME_BILD, Integer.toString(atmen_bild)); //siehe GIF
         values3.put(COLUMN_NAME_KOERPERTEIL, mRuecken);
         values3.put(COLUMN_NAME_SCHWIERIGKEIT, "2"); //Schwierigkeit als Zahl - 1 = Leicht, 2 = Mittel, 3 = Schwer
         values3.put(COLUMN_NAME_WIEDERHOLUNG, mTrue); // Wenn die Übung mit Wiederholungen ausgeübt wird (wie z.B. Kniebeugen) = true; wenn nicht (z.B. Spazieren) = false
@@ -129,13 +129,13 @@ public class DBHelper extends SQLiteOpenHelper {
         db.insert(TABLE_WORKOUTS, null, values3);
 
         //Übung 4
-        int bild4 = R.drawable.fitwork_beispielbild;
-        int gif4 = R.drawable.bsp_gif2;
+        int baum_liegestuetz_bild = R.drawable.baum_liegestuetz_bild;
+        int baum_liegestuetze_gif = R.drawable.baum_liegestuetze_gif;
         ContentValues values4 = new ContentValues();
-        values4.put(COLUMN_NAME_UEBUNGSNAME, "Übung 4");
+        values4.put(COLUMN_NAME_UEBUNGSNAME, "Baum Liegestütze");
         values4.put(COLUMN_NAME_UEBUNGSBESCHREIBUNG, "Blah");
-        values4.put(COLUMN_NAME_GIF, Integer.toString(gif4)); //echt auf Tippfehler aufpassen - am besten Copy&Paste vom Namen und dann ".gif" löschen
-        values4.put(COLUMN_NAME_BILD, Integer.toString(bild4)); //siehe GIF
+        values4.put(COLUMN_NAME_GIF, Integer.toString(baum_liegestuetze_gif)); //echt auf Tippfehler aufpassen - am besten Copy&Paste vom Namen und dann ".gif" löschen
+        values4.put(COLUMN_NAME_BILD, Integer.toString(baum_liegestuetz_bild)); //siehe GIF
         values4.put(COLUMN_NAME_KOERPERTEIL, mArme);
         values4.put(COLUMN_NAME_SCHWIERIGKEIT, "2"); //Schwierigkeit als Zahl - 1 = Leicht, 2 = Mittel, 3 = Schwer
         values4.put(COLUMN_NAME_WIEDERHOLUNG, mTrue); // Wenn die Übung mit Wiederholungen ausgeübt wird (wie z.B. Kniebeugen) = true; wenn nicht (z.B. Spazieren) = false
@@ -143,8 +143,8 @@ public class DBHelper extends SQLiteOpenHelper {
         db.insert(TABLE_WORKOUTS, null, values4);
 
         //Übung 5
-        int bild5 = R.drawable.fitwork_beispielbild;
-        int gif5 = R.drawable.bsp_gif;
+        int bild5 = R.drawable.beinschwung_bild;
+        int gif5 = R.drawable.beinschwung;
         ContentValues values5 = new ContentValues();
         values5.put(COLUMN_NAME_UEBUNGSNAME, "Übung 5");
         values5.put(COLUMN_NAME_UEBUNGSBESCHREIBUNG, "Blah");
@@ -157,8 +157,8 @@ public class DBHelper extends SQLiteOpenHelper {
         db.insert(TABLE_WORKOUTS, null, values5);
 
         //Übung 6
-        int bild6 = R.drawable.fitwork_beispielbild;
-        int gif6 = R.drawable.bsp_gif;
+        int bild6 = R.drawable.dehnen_bild;
+        int gif6 = R.drawable.dehnen_gif;
         ContentValues values6 = new ContentValues();
         values6.put(COLUMN_NAME_UEBUNGSNAME, "Übung 6");
         values6.put(COLUMN_NAME_UEBUNGSBESCHREIBUNG, "Blah");
@@ -171,8 +171,8 @@ public class DBHelper extends SQLiteOpenHelper {
         db.insert(TABLE_WORKOUTS, null, values6);
 
         //Übung 7
-        int bild7 = R.drawable.fitwork_beispielbild;
-        int gif7 = R.drawable.bsp_gif2;
+        int bild7 = R.drawable.handschalgen_bild;
+        int gif7 = R.drawable.handschlagen_gif;
         ContentValues values7 = new ContentValues();
         values7.put(COLUMN_NAME_UEBUNGSNAME, "Übung 7");
         values7.put(COLUMN_NAME_UEBUNGSBESCHREIBUNG, "Blah");
@@ -185,8 +185,8 @@ public class DBHelper extends SQLiteOpenHelper {
         db.insert(TABLE_WORKOUTS, null, values7);
 
         //Übung 8
-        int bild8 = R.drawable.fitwork_beispielbild;
-        int gif8 = R.drawable.bsp_gif;
+        int bild8 = R.drawable.rueckensitzen_bild;
+        int gif8 = R.drawable.rueckensitzen_gif;
         ContentValues values8 = new ContentValues();
         values8.put(COLUMN_NAME_UEBUNGSNAME, "Übung 8");
         values8.put(COLUMN_NAME_UEBUNGSBESCHREIBUNG, "Blah");
@@ -199,8 +199,8 @@ public class DBHelper extends SQLiteOpenHelper {
         db.insert(TABLE_WORKOUTS, null, values8);
 
         //Übung 9
-        int bild9 = R.drawable.fitwork_beispielbild;
-        int gif9 = R.drawable.bsp_gif;
+        int bild9 = R.drawable.trizeps_bild;
+        int gif9 = R.drawable.trizeps_gif;
         ContentValues values9 = new ContentValues();
         values9.put(COLUMN_NAME_UEBUNGSNAME, "Übung 9");
         values9.put(COLUMN_NAME_UEBUNGSBESCHREIBUNG, "Blah");
@@ -213,8 +213,9 @@ public class DBHelper extends SQLiteOpenHelper {
         db.insert(TABLE_WORKOUTS, null, values9);
 
         //Übung 10
-        int bild10 = R.drawable.fitwork_beispielbild;
-        int gif10 = R.drawable.bsp_gif;
+        int bild10 = R.drawable.werfen_bild;
+        int gif10 = R.drawable.werfen_gif;
+        int gif11 = R.drawable.werfen_variation_gif;
         ContentValues values10 = new ContentValues();
         values10.put(COLUMN_NAME_UEBUNGSNAME, "Übung 10");
         values10.put(COLUMN_NAME_UEBUNGSBESCHREIBUNG, "Blah");
